046cb7e35c620edd2cc13d1337fb30a67220230c
Matthew Black <mblackmblackmblack@gmail.com>
2022-10-29 22:45:38 -0400

    ✨ feat: add default timeout for clients

--- packages/clients/lib/options.ts
+++ packages/clients/lib/options.ts
@@ -4,6 +4,7 @@ export type ApiPrefix = 'api';
 export interface IOptions {
   uri: string;
   logger: Logger;
+  timeout?: number;
   apiKey?: string;
   headers?: [string, string][];
   prefix?: string;
